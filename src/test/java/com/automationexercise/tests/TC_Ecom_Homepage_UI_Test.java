9126694df3a62e78a2cdeb95c6fd5fa9be240fc3
Sneha N <134149209+snehanagarajgowda@users.noreply.github.com>
2025-09-08 15:37:16 +0530

    Test

--- src/test/java/com/automationexercise/tests/TC_Ecom_Homepage_UI_Test.java
+++ src/test/java/com/automationexercise/tests/TC_Ecom_Homepage_UI_Test.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -17,16 +20,26 @@ import com.automationexercise.utilities.ScreenshotUtilities;
 import com.aventstack.extentreports.ExtentTest;
 
 public class TC_Ecom_Homepage_UI_Test extends BaseTest{
-	String projectpath=System.getProperty("user.dir");
+	TC_Ecom_Homepage_UI homepage;
+
+	@BeforeMethod
+	public void setupPage() {
+		homepage = new TC_Ecom_Homepage_UI(driver);
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		if (extent != null) {
+			extent.flush();
+		}
+	}
 
 	//TC01. To verify and validate the E-commerece web application  URL is working on chrome browsers.
 	@Test
 	public void TC01_NavigateHomepage() {
 		ExtentTest test = extent.createTest("Verify url opening homepage on browser");
-		driver.get("https://automationexercise.com");
-		
 		try {
-			
+			driver.get("https://automationexercise.com");
 			String actualURL = driver.getCurrentUrl();
 			String actualTitle = driver.getTitle();
 			if (actualURL.equals("https://automationexercise.com/") && actualTitle.equalsIgnoreCase("Automation Exercise")) {
@@ -44,9 +57,6 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	@Test
 	public void TC02_verifyLogo() {
 		ExtentTest test = extent.createTest("verify Logo");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 		try {
 			boolean result = homepage.isLogoDisplayed();
 			if(result) {
@@ -64,16 +74,26 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 		}
 
 	
+	//TC03. To verify and validate the  E-commerece web application home button on home page
+	@Test
+	public void TC03_verifyHomeButton() throws IOException {
+		ExtentTest test = extent.createTest("verify Home button on home page");
+		try {
+			homepage.clickHomeButton();
+			test.pass("Home button clicked successfully");
+		}
+			 catch(Exception e) {
+					test.fail("Home button is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "Home button not clicked"));;
+		}
+			
+		}
 	//TC04. To verify and validate the E-commerece web application home logo is displayed on home page
 		@Test
 		public void TC04_verifyHomeButtonlogo() {
-			TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-			driver.get("https://automationexercise.com");
-
-			ExtentTest test = extent.createTest("verify Home logo in home page");
+			ExtentTest test = extent.createTest("verify Home logo on home page");
 			try {
 				 if (homepage.isHomeButtonDisplayed()) {
-			            test.pass("Home button is displayed on the homepage");
+			            test.pass("Home logo is displayed on the homepage");
 			            homepage.clickHomeButton();
 			            test.pass("Home button clicked successfully");
 			        } else {
@@ -90,9 +110,6 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	@Test
 	public void TC05_verifyProductButton() throws IOException {
 		ExtentTest test = extent.createTest("verify products button clickability");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 		try {
 			homepage.clickProductButton();
 			test.pass("Product button clicked successfully");
@@ -107,9 +124,6 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	@Test
 	public void TC06_verifyCartButton() throws IOException {
 		ExtentTest test = extent.createTest("verify cart button clickability");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 		try {
 			homepage.clickCartButton();
 			test.pass("Cart button clicked successfully");
@@ -123,9 +137,6 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	@Test
 	public void TC07_verifyLogin_SignUpButton() throws IOException {
 		ExtentTest test = extent.createTest("verify signup/login button clickability");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 		try {
 			homepage.clickLoginSignup();
 			test.pass("Login_Signup button clicked successfully");
@@ -135,15 +146,23 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 		}
 			
 		} 
-	
-	
+	//TC08 To verify and validate the  E-commerece web application Test cases button on home page.
+	@Test
+	public void TC08_verifyTestCases() throws IOException {
+		ExtentTest test = extent.createTest("verify Test cases button clickability");
+		try {
+			homepage.clickTestCases();
+			test.pass("Test Cases button clicked successfully");
+		}
+			 catch(Exception e) {
+					test.fail("Test Cases button is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "Test Cases button not clicked"));;
+		}
+			
+		} 
 	//TC09. To verify and validate the E-commerece web application API Testing button on home page.
 	@Test
 	public void TC09_verifyAPITesting() throws IOException {
 		ExtentTest test = extent.createTest("verify API testing button clickability");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 		try {
 			homepage.clickAPITesting();
 			test.pass("API Testing button clicked successfully");
@@ -157,16 +176,13 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	//TC.10 To verify and validate the  E-commerece web application Vedio Tutorials button on home page.
 	@Test
 	public void TC10_verifyVideoTutorials() throws IOException {
-		ExtentTest test = extent.createTest("verify and validate vedio tutorials button clickability");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
+		ExtentTest test = extent.createTest("verify and validate video tutorials button clickability");
 		try {
 			homepage.clickVideoTutorials();
 			test.pass("Vedio Tutorials button clicked successfully");
 		}
 			 catch(Exception e) {
-					test.fail("Vedio Tutorials  button is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "Vedio Tutorials button not clicked"));;
+					test.fail("Vedio Tutorials  button is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "Vedio Tutorials button not clicked"));
 		}
 			
 		} 
@@ -174,58 +190,35 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	@Test
 	public void TC11_verifyContactUs() throws IOException {
 		ExtentTest test = extent.createTest("verify and validate Contact Us button clickability");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 		try {
 			homepage.clickContactUs();
 			test.pass("Contact Us button clicked successfully");
 		}
 			 catch(Exception e) {
-					test.fail("Contact Us button is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "Contact Us button not clicked"));;
+					test.fail("Contact Us button is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "Contact Us button not clicked"));
 		}
 			
 		} 
 	
-	//TC08 To verify and validate the  E-commerece web application Test cases button on home page.
+	
+	//TC12. To verify and validate the  E-commerece web application APIs list for product on home page.
 	@Test
-	public void TC08_verifyTestCases() throws IOException {
-		ExtentTest test = extent.createTest("verify Test cases button clickability");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
-		try {
-			homepage.clickTestCases();
-			test.pass("Test Cases button clicked successfully");
-		}
-			 catch(Exception e) {
-					test.fail("Test Cases button is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "Test Cases button not clicked"));;
-		}
-			
-		} 
-	//TC13. To verify and validate the  E-commerece web application APIs list for product on home page.
-	@Test
-	public void TC13_verifyAPIListProduct() throws IOException {
-		ExtentTest test = extent.createTest("To verify and validate the  E-commerece web application APIs list for product on home page.");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
+	public void TC12_verifyAPIListProduct() throws IOException {
+		ExtentTest test = extent.createTest("verify and validate side bar on home page");
 		try {
 			homepage.clickAPITestPractice();
-			test.pass("Clicked successfully on api test practice");
+			test.pass("API list for products is clicked");
+		} catch(Exception e) {
+			test.fail("API list for products is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "API List for products is not clicked"));
 		}
-		catch(Exception e) {
-			test.fail("API test practice is not clicked").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "API test practice is not clicked"));;
-}
+		
+		
 	} 
 	
-	//TC14. To verify and validate the  E-commerece web application Category(Ex: Women,men,kids) on home page.
+	//TC13. To verify and validate the  E-commerece web application Category(Ex: Women,men,kids) on home page.
 	@Test
-	public void TC14_verifyCategories() throws IOException {
+	public void TC13_verifyCategories() throws IOException {
 	    ExtentTest test = extent.createTest("Verify Categories on Homepage");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 	    try{
 	    	String[] categories = {"Women", "Men", "Kids"};
 	    	for (String categoryName : categories) {
@@ -238,13 +231,10 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	    }
 	}  
 	
-	//TC15. To verify and validate the  E-commerece web application Brands name(Ex: H&M) on home page.
+	//TC14. To verify and validate the  E-commerece web application Brands name(Ex: H&M) on home page.
 	@Test
-	public void TC15_verifyBrands() throws IOException {
+	public void TC14_verifyBrands() throws IOException {
 	    ExtentTest test = extent.createTest("Verify brands on Homepage");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 	    try {
 	    	String[] brands = {"H&M", "Polo", "MADAME"};
 
@@ -257,13 +247,10 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	    } 
 	} 
 	
-	//TC16. To verify and validate the  E-commerece web application Feature items on home page.
+	//TC15. To verify and validate the  E-commerece web application Feature items on home page.
 	@Test
-	public void TC16_verifyandClickFeaturedItems() throws IOException {
+	public void TC15_verifyandClickFeaturedItems() throws IOException {
 	    ExtentTest test = extent.createTest("Verify and Click Featured Items on Homepage");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 	    try {
 	        for (int i = 1; i <= 3; i++) {
 	            homepage.clickFeaturedItem(i);
@@ -273,17 +260,13 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	        test.fail("Featured item verification failed: " + e.getMessage()).addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "click on feature item failed"));
 	    }
 	} 
-	//TC17. To verify and validate the  E-commerece web application Add to cart icon is visible on home page.
+	//TC16. To verify and validate the  E-commerece web application Add to cart icon is visible on home page.
 	@Test
-	public void TC17_verifyAddCartVisible() {
-	    ExtentTest test = extent.createTest("Verify add to cart is visible on home page");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
-	    try {
-	    	homepage.ClickOnViewProduct();
-	    	WebElement result = homepage.ClickOnAddCart();
-	    	if(result.getText().contains("Availability:")) {
+	public void TC16_verifyAddCartVisible() {
+		ExtentTest test = extent.createTest("Verify add to cart is visible on home page");
+		try {
+	    	//boolean result = homepage.isDisplayedAddCart();
+	    	if(homepage.isDisplayedAddCart()) {
 	    		test.pass("Add to cart is displayed");
 	    	}
 	    	else {
@@ -291,20 +274,17 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	    	}
 	    } catch(Exception e) {
 	    	test.fail("Exception: " + e.getMessage());
-	    }
-	    	
 	    } 
-	//TC18. To verify and validate the  E-commerece web application Cost under product is displayed on home page.
+	}
+	    	
+	    
+	//TC17. To verify and validate the  E-commerece web application Cost under product is displayed on home page.
 	@Test
-	public void TC18_verifycostproductisVisible() {
+	public void TC17_verifycostproductisVisible() {
 	    ExtentTest test = extent.createTest("Verify cost of product visible on home page");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
 	    try {
-	    	homepage.ClickOnViewProduct();
-	    	WebElement result = homepage.ClickOnAddCart();
-	    	if(result.getText().contains("Availability:")) {
+	    	boolean result = homepage.isDisplayedProductCost();
+	    	if(result) {
 	    		test.pass("Product cost is displayed");
 	    	}
 	    	else {
@@ -315,50 +295,6 @@ public class TC_Ecom_Homepage_UI_Test extends BaseTest{
 	    }
 	    	
 	    } 
-	
-	//TC19. To verify and validate the  E-commerece web application view  product is displayed under product on home page.
-	@Test
-	public void TC19_verifyViewItemVisible() {
-	    ExtentTest test = extent.createTest("Verify view item is visible on home page");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
-	    try {
-	    	WebElement result = homepage.ViewProductVisible();
-	    	if(result.getText().contains("View Product")) {
-	    		test.pass("View product is visible");
-	    	}
-	    	else {
-	    		test.fail("view item is not visible").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "view product is not visible"));
-	    	}
-	    } catch(Exception e) {
-	    	test.fail("Exception: " + e.getMessage());
-	    }
-	    	
-	    } 
-	//TC20. To verify and validate the  E-commerece web application by clicking on view products that re directs to detailed product view on home page.
-	@Test
-	public void TC20_verifyViewProductRedirects() {
-	    ExtentTest test = extent.createTest("Verify clicking on View Product redirects to product detail page");
-		TC_Ecom_Homepage_UI homepage = new TC_Ecom_Homepage_UI(driver);
-		driver.get("https://automationexercise.com");
-
-	    try {
-	    	 homepage.clickFeaturedItemByName(); 
-	         String expectedUrl = "https://automationexercise.com/product_details/1"; // example
-	         String actualUrl = driver.getCurrentUrl();
-	         if(actualUrl.equals(expectedUrl)) {
-	             test.pass("Successfully redirected to product detail page");
-	         } else {
-	             test.fail("Redirection failed").addScreenCaptureFromPath(ScreenshotUtilities.capturescreenshot(driver, "ViewProductRedirectFail"));
-	         }
-	     } catch(Exception e) {
-	         test.fail("Exception: " + e.getMessage());
-	     }
-	 
-	    } 
-	
-	
 }
 	
 	
