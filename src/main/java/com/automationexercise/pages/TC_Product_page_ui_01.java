ced9eba730b1833275aad8085068e02455c78cab
tejaswanioleti <oletitejaswani2003@gmail.com>
2025-09-15 09:25:26 +0530

    updated

---
+++ src/main/java/com/automationexercise/pages/TC_Product_page_ui_01.java
@@ -0,0 +1,81 @@
+package com.automationexcercise.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class TC_Product_page_ui_01 {
+    WebDriver driver;
+
+    
+    By logo = By.xpath("//*[@id=\"header\"]/div/div/div/div[1]/div/a/img");
+    By homeButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[1]/a");
+    By productsButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[2]/a");
+    By cartButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[3]/a");
+    By signupLoginButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a");
+    By testCasesButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[5]/a");
+    By apiTestingButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[6]/a");
+    By videoTutorialsButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[7]/a");
+    By contactUsButton = By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[8]/a");
+    By productpageheading=By.xpath("/html/body/section[2]/div/div/div[2]/div/h2");
+    By productlogo=By.xpath("//*[@id=\"header\"]/div/div/div/div[1]/div/a/img");
+    public boolean isproductslogodisplayed() {
+    	return driver.findElement(productlogo).isDisplayed();
+    }
+    public boolean isproductspageheadingdisplayed() {
+    	return driver.findElement(productpageheading).isDisplayed();
+    }
+    public void clickproductbtn() {
+    	driver.findElement(productsButton).click();
+    }
+
+    public TC_Product_page_ui_01(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Validation methods (return true if element is displayed)
+    public boolean isLogoDisplayed() {
+        return driver.findElement(logo).isDisplayed();
+    }
+
+    public boolean isHomeDisplayed() {
+        return driver.findElement(homeButton).isDisplayed();
+    }
+
+    public boolean isProductsDisplayed() {
+        return driver.findElement(productsButton).isDisplayed();
+    }
+
+    public boolean isCartDisplayed() {
+        return driver.findElement(cartButton).isDisplayed();
+    }
+
+    public boolean isSignupLoginDisplayed() {
+        return driver.findElement(signupLoginButton).isDisplayed();
+    }
+
+    public boolean isTestCasesDisplayed() {
+        return driver.findElement(testCasesButton).isDisplayed();
+    }
+
+    public boolean isApiTestingDisplayed() {
+        return driver.findElement(apiTestingButton).isDisplayed();
+    }
+
+    public boolean isVideoTutorialsDisplayed() {
+        return driver.findElement(videoTutorialsButton).isDisplayed();
+    }
+
+    public boolean isContactUsDisplayed() {
+        return driver.findElement(contactUsButton).isDisplayed();
+    }
+ 
+    public boolean isVerticalScrollBarPresent() {
+        driver.findElement(By.xpath("//a[@href='/products']")).click(); // open Products page
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return (Long) js.executeScript("return document.body.scrollHeight;") >
+               (Long) js.executeScript("return window.innerHeight;");
+    }
+
+}
+
---
+++ src/main/java/com/automationexercise/pages/TC_Register_page_func_02.java
@@ -0,0 +1,205 @@
+package com.automationexcercise.pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TC_Register_page_func_02 {
+    WebDriver driver;
+
+    public TC_Register_page_func_02(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Locators
+    By loginSignupBtn = By.xpath("//a[contains(text(),'Signup / Login')]");
+    By signupUsername = By.xpath("//input[@name='name']");
+    By signupEmail = By.xpath("//input[@data-qa='signup-email']");
+    By companyInput = By.xpath("//*[@id=\"company\"]");
+    By address1Input = By.id("address1");
+    By address2Input = By.id("address2");
+    By countryDropdown = By.id("country");
+    By stateInput = By.id("state");
+    By cityInput = By.id("city");
+    By zipcodeInput = By.id("zipcode");
+    By mobileNumberInput = By.id("mobile_number");
+    By companyTextbox = By.id("company");
+    By signupButton = By.xpath("//button[@data-qa='signup-button']");
+    By homepageheading=By.xpath("//*[@id=\"slider-carousel\"]/div/div[1]/div[1]/h1"); 
+    By signupheading=By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/h2");	
+    By registerHeading = By.xpath("//b[contains(text(),'Enter Account Information')]");
+    public boolean registerpageheadingisdisplayed() {
+        try {
+            return driver.findElement(registerHeading).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+   public boolean issignuppageheadingDisplayed() {
+	return driver.findElement(signupheading).isDisplayed();
+   }
+    public boolean ishomepageheadingDisplayed() {
+	return driver.findElement(homepageheading).isDisplayed();
+    }
+    public void clicksignupbutton() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement button = wait.until(ExpectedConditions.elementToBeClickable(signupButton));
+        try {
+            button.click();
+        } catch (Exception e) {
+            // fallback: JavaScript click
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", button);
+        }
+    }
+    // **Add this getter method**
+    public String get_company() {
+        return driver.findElement(companyTextbox).getAttribute("value");
+    }
+
+    // Click Login/Signup
+    public void clickloginsignupbutton() {
+        driver.findElement(loginSignupBtn).click();
+    }
+
+    // Enter username
+    public void enterusername(String name) {
+        driver.findElement(signupUsername).clear();
+        driver.findElement(signupUsername).sendKeys(name);
+    }
+
+    // Enter email
+    public void enteremail(String email) {
+        driver.findElement(signupEmail).clear();
+        driver.findElement(signupEmail).sendKeys(email);
+    }
+
+
+    // Company
+    public boolean enter_company(String company) {
+        try {
+            WebElement ele = driver.findElement(companyInput);
+            ele.clear();
+            if (company.matches("[a-zA-Z0-9\\s]+") || company.isEmpty()) {
+                ele.sendKeys(company);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+      
+    }
+    // Address1
+    public boolean enter_address1(String address1) {
+        try {
+            WebElement ele = driver.findElement(address1Input);
+            ele.clear();
+            if (address1.matches("[a-zA-Z0-9\\s]+") || address1.isEmpty()) {
+                ele.sendKeys(address1);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // Address2
+    public boolean enter_address2(String address2) {
+        try {
+            WebElement ele = driver.findElement(address2Input);
+            ele.clear();
+            if (address2.matches("[a-zA-Z0-9\\s]+") || address2.isEmpty()) {
+                ele.sendKeys(address2);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // Country
+    public boolean select_country(String country) {
+        try {
+            Select sel = new Select(driver.findElement(countryDropdown));
+            sel.selectByVisibleText(country);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // State
+    public boolean enter_state(String state) {
+        try {
+            WebElement ele = driver.findElement(stateInput);
+            ele.clear();
+            if (state.matches("[a-zA-Z\\s]+") || state.isEmpty()) {
+                ele.sendKeys(state);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // City
+    public boolean enter_city(String city) {
+        try {
+            WebElement ele = driver.findElement(cityInput);
+            ele.clear();
+            if (city.matches("[a-zA-Z\\s]+") || city.isEmpty()) {
+                ele.sendKeys(city);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // Zipcode
+    public boolean enter_zipcode(String zipcode) {
+        try {
+            WebElement ele = driver.findElement(zipcodeInput);
+            ele.clear();
+            if (zipcode.matches("\\d+") || zipcode.isEmpty()) { // only digits allowed
+                ele.sendKeys(zipcode);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    // Mobile Number
+    public boolean enter_mobileNumber(String mobile) {
+        try {
+            WebElement ele = driver.findElement(mobileNumberInput);
+            ele.clear();
+            if (mobile.matches("\\d+") || mobile.isEmpty()) { // only digits allowed
+                ele.sendKeys(mobile);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
